546e0aba36cfa4e99a3c791e9748d5c9275999dd
Marcia <marciaadler718@gmail.com>
2020-03-12 09:01:01 -0400

    added formatting to search page

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -2,18 +2,21 @@ import React from "react";
 
 export default function Books(props) {
   return (
-    <div>
-      <ul>
-        <li>{props.title}</li>
-        <li>{props.authors}</li>
-        <li>{props.description}</li>
-        <li>
-          <img alt={props.title} src={props.image} />
-        </li>
-        <li>
-          <a href={props.link}>Link</a>
-        </li>
-      </ul>
+    <div className="row">
+      <div className="col-12 col-md-3 col-lg-3">
+        <img alt={props.title} src={props.image} />
+        <button>Save</button>
+      </div>
+      <div className="col-12 col-md-9 col-lg-9">
+        <ul>
+          <li>{props.title}</li>
+          <li>{props.authors}</li>
+          <li>{props.description}</li>
+          <li>
+            <a href={props.link}>Link</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
