18785c3e39fa54cfaff188d78a6b1a325978c4a1
Marcia <marciaadler718@gmail.com>
2020-03-12 09:08:39 -0400

    additional formating for search list

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -2,20 +2,25 @@ import React from "react";
 
 export default function Books(props) {
   return (
-    <div className="row">
-      <div className="col-12 col-md-3 col-lg-3">
-        <img alt={props.title} src={props.image} />
-        <button>Save</button>
-      </div>
-      <div className="col-12 col-md-9 col-lg-9">
-        <ul>
-          <li>{props.title}</li>
-          <li>{props.authors}</li>
-          <li>{props.description}</li>
-          <li>
-            <a href={props.link}>Link</a>
-          </li>
-        </ul>
+    <div>
+      <div className="row">
+        <div className="col-12 col-md-3 col-lg-3">
+          <img className="ml-3 mt-3" alt={props.title} src={props.image} />
+          <br />
+          <button className="ml-3">Save</button>
+        </div>
+        <div className="col-12 col-md-9 col-lg-9">
+          <ul className="mt-3">
+            <li>
+              <strong>{props.title}</strong>
+            </li>
+            <li>{props.authors}</li>
+            <li>{props.description}</li>
+            <li>
+              <a href={props.link}>Link</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
